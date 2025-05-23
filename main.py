173c3c26e175c0203388607da4c378b44cf4213b
Hannes <hannes-bachmann@t-online.de>
2025-05-24 00:19:24 +0200

    split requests

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data_loading import load_srt_from_file
-from model_api import model_request
+from model_api import get_summary, get_mood, get_speakers, get_references
 from data_processing import combine_subs_into_sentences, split_by_pauses
 
 
@@ -10,9 +10,24 @@ def run_all():
     sentences = combine_subs_into_sentences(srt_data)
     scenes = split_by_pauses(sentences, pause_length=4)
     # process using a LLM model via API
+    constructed_output = []
     for scene in scenes:
-        response = model_request(scene['content'])
-        print(response)
+        # constructed_output.append(
+        #     {
+        #      "start": "00:00:22,719",
+        #      "end": "00:00:31,507",
+        #      "transcript": ,
+        #      "summary": ,
+        #      "characters": [],
+        #      "mood": ,
+        #      "cultural_refs": []
+        #     })
+        mood = get_mood(scene['content'])
+        summary = get_summary(scene['content'])
+        speakers = get_speakers(scene['content'])
+        cultural_references = get_references(scene['content'])
+        print(mood)
+        pass
     pass
 
 
